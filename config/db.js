088c52e1eee1c1b33c29e9289186887dcd919ce1
Rendy Artha <rendyartha312@gmail.com>
2021-03-24 11:57:50 +0700

    install env

--- config/db.js
+++ config/db.js
@@ -1,10 +1,11 @@
 const {Sequelize} = require('sequelize')
+require("dotenv").config()
 
 const sequelize = new Sequelize({
-  host: "localhost",
-  username: "root",
-  password: "",
-  database: "testing-sequelize",
+  host: process.env.DATABASE_HOST,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASS,
+  database: process.env.DATABASE_NAME,
   dialect: "mysql"
 })
 
