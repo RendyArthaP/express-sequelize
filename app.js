ff63bae3c9824f0494b6ff616f34b792cfe252f7
Rendy Artha <rendyartha312@gmail.com>
2021-03-24 10:16:38 +0700

    setup connection sequelize

--- app.js
+++ app.js
@@ -1,10 +1,22 @@
 const express = require('express');
-const app = expres()
+const app = express()
 const port = process.env.port || 3000
 const routes = require('./routes')
+const sequelize = require('./config/db');
 
 app.use(express.json())
 app.use(routes)
+
+async function testConnection() {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+testConnection()
 
 app.listen(port, (req, res) => {
   console.log(`Application running at http://localhost:${port}`)
